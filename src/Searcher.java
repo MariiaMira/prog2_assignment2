72273d374b88da0128b81520ee54458b99bc8fa8
MariiaMira <roze2117@gmail.com>
2024-04-09 14:44:03 +0200

    addAll metod

--- src/Searcher.java
+++ src/Searcher.java
@@ -16,9 +16,7 @@ public class Searcher implements SearchOperations{
 		for (Recording recording : recordings) {
 			artists.add(recording.getArtist());
 			titles.add(recording.getTitle());
-			for(String g: recording.getGenre()){
-				genres.add(g);
-			}
+            genres.addAll(recording.getGenre());
 		}
 
 	}
